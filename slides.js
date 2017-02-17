30c831c72c543c3c0305f3444beff92f10f34a3e
Bodil Stokke <bodil@bodil.org>
2017-02-17 21:46:24 +0000

    Make it so.

---
+++ slides.js
@@ -0,0 +1,18 @@
+import Pink from "pink";
+import "pink/css/themes/simon.less";
+import "pink/node_modules/highlight.js/styles/github-gist.css";
+import "./screen.less";
+import background from "pink/modules/background";
+import image from "pink/modules/image";
+import highlight from "pink/modules/highlight";
+import repl from "pink-repl";
+import js from "pink-repl/javascript";
+import typescript from "pink-repl-typescript";
+import purescript from "pink-repl-purescript";
+
+new Pink("#slides", {
+  background,
+  image,
+  highlight,
+  repl: repl({js, typescript, purescript})
+});
